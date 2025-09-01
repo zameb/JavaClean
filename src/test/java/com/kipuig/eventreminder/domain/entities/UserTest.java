c457d4bcc301368dd4e05dc75c66e1148d96c8e3
Zameb <zameb@hotmail.com>
2025-09-01 11:08:47 -0500

    Domain User tests

--- src/test/java/com/kipuig/eventreminder/domain/entities/UserTest.java
+++ src/test/java/com/kipuig/eventreminder/domain/entities/UserTest.java
@@ -8,9 +8,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
-
     @Test
-    void freePlanShouldRejectSubscriptionsOutOfTheLimit() throws InvalidInitializationException, SubscriptionsLimitException {
+    void userShouldRejectSubscriptionsOutOfThePlanLimit() throws InvalidInitializationException, SubscriptionsLimitException {
         var planType = new PlanType(null, "FREE", 3);
         var user = new User(null, "t@e.com", planType);
         var dateTime = ZonedDateTime.now(ZoneId.of("UTC"));
@@ -23,6 +22,35 @@ public class UserTest {
 
         var exception = assertThrows(SubscriptionsLimitException.class,
                 () -> user.addSubscription(anotherSubscription)
+        );
+
+        assertFalse(exception.getMessage().isEmpty());
+    }
+
+    @Test
+    void userWithoutEmailIsRejected() {
+        var planType = new PlanType(null, "FREE", 3);
+        var exception = assertThrows(InvalidInitializationException.class,
+                () -> new User(null, planType)
+        );
+
+        assertFalse(exception.getMessage().isEmpty());
+    }
+
+    @Test
+    void userWithInvalidEmailIsRejected() {
+        var planType = new PlanType(null, "FREE", 3);
+        var exception = assertThrows(InvalidInitializationException.class,
+                () -> new User("invalid_email_value", planType)
+        );
+
+        assertFalse(exception.getMessage().isEmpty());
+    }
+    
+    @Test
+    void userWithoutPlanTypeIsRejected() {
+        var exception = assertThrows(InvalidInitializationException.class,
+                () -> new User("valid_email@hotmail.com", null)
         );
 
         assertFalse(exception.getMessage().isEmpty());
